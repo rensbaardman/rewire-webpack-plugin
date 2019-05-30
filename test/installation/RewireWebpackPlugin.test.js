55160f32a480121d7c1644d11327f9f2a0f8730d
Rens Baardman <git@rensbaardman.nl>
2019-05-30 19:27:26 +0200

    Add boilerplate for installation test

---
+++ test/installation/RewireWebpackPlugin.test.js
@@ -0,0 +1,15 @@
+// This is a sanity check to make sure all dependencies
+// have been declared properly, and installation
+// ('npm install rewire-webpack-plugin') goes smoothly.
+
+const assert = require('chai').assert;
+
+describe('RewireWebpackPlugin', () => {
+
+	it('should install without warnings', () => {
+		assert.fail('finish the test')
+		// see e.g. https://docs.npmjs.com/cli/link on how
+		// to install a module locally
+	})
+
+})
